c112c07735798e79839a93e464f528417ff68fad
Davide <davide97ls@gmail.com>
2020-10-16 13:22:13 +0800

    removed some files

---
+++ compute_score.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import argparse
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+
+
+def rmse(preds, labels):
+    return sqrt(mean_squared_error(preds, labels))
+
+
+def compute_score(args):
+    path_data = args.path_data
+    path_validation = args.path_val
+
+    df1 = pd.read_csv(path_data, encoding='ISO-8859-1')["price"].values
+    df2 = pd.read_csv(path_validation, encoding='ISO-8859-1', header=None).values
+    score = rmse(df1, df2)
+    print(score)
+
+    print("done!")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_data', type=str)
+    parser.add_argument('--path_val', type=str)
+    args = parser.parse_known_args()[0]
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    compute_score(args)
--- extra.py
+++
@@ -1,8 +0,0 @@
-# fill missing values
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(fill_value=np.nan, startegy='mean')
-X = imputer.fit_transform(df)
-# Because it returns a numpy array, to read it, we can convert it back to the data frame.
-
-# drop missing values
-dropedDf = df.dropna()
